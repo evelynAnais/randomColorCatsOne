bb3e6bc5fc75ed63ac289f8f20acdfc9faf8f75c
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-06-19 16:55:02 -0600

    style and formatting changes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const BASE_URL = 'https://cat-fact.herokuapp.com/facts' 
+const BASE_URL = 'https://cat-fact.herokuapp.com/facts';
 
-let catFacts
+let catFacts;
 async function getCatFact() {
   try {
-    const response = await axios.get(BASE_URL)
+    const response = await axios.get(BASE_URL);
     return response.data.map((fact) => {
-      return fact.text
+      return fact.text;
     });
   } catch(error) {
-    console.log(error)
+    console.log(error);
   }
 }
 getCatFact().then((data) => catFacts = data);
@@ -19,20 +19,20 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.catfact');
 
 btn.addEventListener('click', function() {
-  getCatFact()
+  getCatFact();
 
   let hexColor = '#'; 
   for(let i = 0; i < 6; i++) {
-    hexColor += hex[getRandomNumber()]
+    hexColor += hex[getRandomNumber()];
   }
-  color.textContent = catFacts[randomCat()]
+  color.textContent = catFacts[randomCat()];
   document.body.style.backgroundColor = hexColor;
 });
 
 function randomCat () {
-  return Math.floor(Math.random() * catFacts.length)
+  return Math.floor(Math.random() * catFacts.length);
 }
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * hex.length)
+  return Math.floor(Math.random() * hex.length);
 }
